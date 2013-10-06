51b4e247f85498c2260e741a06046a3b86f578b3
Shannon Wynn <shannonwynn@mac.com>
2013-10-06 12:14:25 -0400

    Initial Commit

---
+++ Method Challenge Solution/SWViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWViewController.h
+//  Method Challenge Solution
+//
+//  Created by Shannon Wynn on 10/6/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWViewController : UIViewController
+
+@end
---
+++ Method Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Method Challenge Solution
+//
+//  Created by Shannon Wynn on 10/6/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
